6482ccd3b06a62b5d250b8437ab9883d0fdfaf06
growdu <growdu@gmail.com>
2018-12-25 11:16:46 +0800

    add gc test

---
+++ GcTest/GCExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GcTest
+{
+    class GCExample:IDisposable
+    {
+        public string gcName { get; set; }
+
+        public void Test()
+        {
+            int hash = gcName.GetHashCode();
+        }
+
+        public void Dispose()
+        {
+            gcName = null;
+        }
+    }
+}
