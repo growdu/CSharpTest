6ae23088b0b1f3832fc97ce657d3b009fcf851c8
growdu <growdu@gmail.com>
2019-03-14 15:08:52 +0800

    add macro test.

---
+++ src/moudle/MacroTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            Console.WriteLine("This is debug");
+#else
+            Console.WriteLine("This is realse");
+#endif
+            Console.ReadKey();
+        }
+    }
+}
