a54d60c0af7910b0fbfe62842a741c15dc879985
growdu <growdu@gmail.com>
2019-03-21 09:02:21 +0800

    add download mail.

---
+++ src/moudle/MailTest/Program.cs
@@ -0,0 +1,69 @@
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailTest
+{
+    /// <summary>
+    /// 邮件测试程序，收发邮件以及下载邮件附件，使用mailkit,mimekit
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建获取邮件客户端并连接到邮件服务器
+            ImapClient client = new ImapClient();
+            //client.Connect("mail.gildata.com");
+            //必须调用该事件获取服务器证书，否则无法正常连接
+            ServicePointManager.ServerCertificateValidationCallback += RemoteCertificateValidate;
+            //带端口号和协议的连接方式
+            client.Connect("imap.gildata.com", 993, true);
+            string account = "YHTX@gildata.com";
+            string passWord = "Hello1234";
+            client.Authenticate(account, passWord);
+            //获取所有文件夹
+            List<IMailFolder> mailFolderList = client.GetFolders(client.PersonalNamespaces[0]).ToList();
+            //获取收件箱文件夹
+            var folder = client.GetFolder("INBOX");
+            //以只读方式打开文件夹
+            folder.Open(MailKit.FolderAccess.ReadOnly);
+            //选出日期在2019-03-14后的邮件
+            var uidss = folder.Search(SearchQuery.DeliveredAfter(DateTime.Parse("2019-03-14")));
+            folder.Fetch(uidss, MessageSummaryItems.UniqueId | MessageSummaryItems.Full);
+            foreach (var item in uidss)
+            {
+                MimeMessage message = folder.GetMessage(new UniqueId(item.Id));
+                foreach (MimePart attachment in message.Attachments)
+                {
+                    //下载附件
+                    using (var cancel = new System.Threading.CancellationTokenSource())
+                    {
+                        string filePath = Path.Combine(".", attachment.FileName);
+                        using (var stream = File.Create(filePath))
+                        {
+                            attachment.ContentObject.DecodeTo(stream, cancel.Token);
+                        }
+                    }
+                }
+                
+            }
+            //关闭文件
+            folder.Close();
+        }
+
+        private static bool RemoteCertificateValidate(object sender, X509Certificate cert, X509Chain chain, SslPolicyErrors error)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/moudle/ObjectTest/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// C sharp 利用反射遍历对象属性测试
+/// </summary>
+namespace ObjectTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test model = new Test();
+            Console.WriteLine(model.ToString());
+            ForeachClassProperties<Test>(model);
+            Test test = new Test();
+            foreach (var propertyInfo in typeof(Test).GetProperties())
+            {
+                Console.WriteLine(propertyInfo.Name);
+            }
+            Console.ReadKey();
+        }
+
+
+        /// <summary>
+        /// C#反射遍历对象属性
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="model">对象</param>
+        public static void ForeachClassProperties<T>(T model)
+        {
+            Type t = model.GetType();
+            PropertyInfo[] PropertyList = t.GetProperties();
+            foreach (PropertyInfo item in PropertyList)
+            {
+                string name = item.Name;
+                object value = item.GetValue(model, null);
+            }
+        }
+
+
+        public class Test:ToS
+        {
+            public Test()
+            {
+                name = "test";
+                password = "test";
+                age = 18;
+            }
+
+            public string name { get; set; }
+
+            public string password { get; set; }
+
+            public int age { get; set; }
+
+            //public override string ToString()
+            //{
+            //    StringBuilder sb = new StringBuilder();
+            //    Type t = this.GetType();
+            //    PropertyInfo[] PropertyList = t.GetProperties();
+            //    foreach (PropertyInfo item in PropertyList)
+            //    {
+            //        object value = item.GetValue(this, null);
+            //        sb.Append(value.ToString());
+            //    }
+            //    return sb.ToString();
+            //}
+        }
+
+        public abstract class ToS
+        {
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                Type t = this.GetType();
+                PropertyInfo[] PropertyList = t.GetProperties();
+                foreach (PropertyInfo item in PropertyList)
+                {
+                    object value = item.GetValue(this, null);
+                    sb.Append(value.ToString());
+                }
+                return sb.ToString();
+            }
+        }
+
+    }
+}
