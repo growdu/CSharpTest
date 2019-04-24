ed048c1176ff06b0c7c0dd4cc68ca7cefe52ce80
growdu <growdu@gmail.com>
2019-04-24 13:32:07 +0800

    add read big file.

---
+++ src/moudle/FileTest/ReadFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FileTest
+{
+ /***********************************************************************
+ * 文 件 名：
+ * CopyRight(C) 2019-2029 https://www.github.com/growdu
+ * 创 建 人：growdu
+ * 创建日期：2019-04-24
+ * 修 改 人：
+ * 修改日期：
+ * 描    述：读取大文件
+ ***********************************************************************/
+    class ReadFile
+    {
+        /// <summary>
+        /// 读取大的txt文件
+        /// </summary>
+        /// <param name="file">文件路径</param>
+        /// <param name="currentLine">从文件的哪一行读取</param>
+        /// <param name="limit">每次读取多少行</param>
+        /// <returns></returns>
+        public static IEnumerable<string> readMultiLines(string file, int currentLine, int limit = 500)
+        {
+            try
+            {
+                var lines = File.ReadLines(file, Encoding.UTF8).Skip(currentLine).Take(limit);
+                return lines;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ src/moudle/InterfaceTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InterfaceTest
+{
+ /***********************************************************************
+* 文 件 名：
+* CopyRight(C) 2019-2029 https://www.github.com/growdu
+* 创 建 人：growdu
+* 创建日期：2019-04-24
+* 修 改 人：
+* 修改日期：
+* 描    述：接口测试，测试类和接口互换
+***********************************************************************/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Interface1 hello = new Hello();
+            hello.Hello();
+            Console.ReadKey();
+        }
+    }
+
+    public class Hello : Interface1
+    {
+        void Interface1.Hello()
+        {
+            Console.WriteLine("Hello.");
+        }
+    }
+
+    public class SortHelper<T> where T : IComparable
+    {
+
+    }
+
+    public class SortHelper2
+    {
+        public void Sort<T>(T[] array) where T : IComparable
+        {
+
+        }
+    }
+
+}
