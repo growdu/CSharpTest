507a6bb45bfb707c3eab3a1f3ca670c945404122
growdu <growdu@gmail.com>
2019-05-31 16:25:34 +0800

    add dataHelper.

---
+++ src/view/DataHelper/frmAdd.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace DataHelper
+{
+    partial class frmAdd
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblConnectionName = new System.Windows.Forms.Label();
+            this.txtConnectionName = new System.Windows.Forms.TextBox();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.lblIp = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblConnectionName
+            // 
+            this.lblConnectionName.AutoSize = true;
+            this.lblConnectionName.Location = new System.Drawing.Point(43, 32);
+            this.lblConnectionName.Name = "lblConnectionName";
+            this.lblConnectionName.Size = new System.Drawing.Size(77, 12);
+            this.lblConnectionName.TabIndex = 0;
+            this.lblConnectionName.Text = "数据库连接名";
+            // 
+            // txtConnectionName
+            // 
+            this.txtConnectionName.Location = new System.Drawing.Point(141, 29);
+            this.txtConnectionName.Name = "txtConnectionName";
+            this.txtConnectionName.Size = new System.Drawing.Size(100, 21);
+            this.txtConnectionName.TabIndex = 1;
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(141, 78);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(100, 21);
+            this.txtHost.TabIndex = 3;
+            // 
+            // lblIp
+            // 
+            this.lblIp.AutoSize = true;
+            this.lblIp.Location = new System.Drawing.Point(43, 81);
+            this.lblIp.Name = "lblIp";
+            this.lblIp.Size = new System.Drawing.Size(65, 12);
+            this.lblIp.TabIndex = 2;
+            this.lblIp.Text = "数据库地址";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(141, 127);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 21);
+            this.txtUser.TabIndex = 5;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(43, 130);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(41, 12);
+            this.lblUser.TabIndex = 4;
+            this.lblUser.Text = "用户名";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(141, 169);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(100, 21);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(43, 178);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(29, 12);
+            this.lblPassword.TabIndex = 6;
+            this.lblPassword.Text = "密码";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(22, 235);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 8;
+            this.btnTest.Text = "测试连接";
+            this.btnTest.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(149, 235);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "关闭";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // frmAdd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 283);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.lblIp);
+            this.Controls.Add(this.txtConnectionName);
+            this.Controls.Add(this.lblConnectionName);
+            this.Name = "frmAdd";
+            this.Text = "新增数据库连接";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblConnectionName;
+        private System.Windows.Forms.TextBox txtConnectionName;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label lblIp;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
