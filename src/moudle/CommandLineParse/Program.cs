38aadee7e97972d71d491d82f3258d5065fe39fe
growdu <growdu@gmail.com>
2019-06-27 18:00:54 +0800

    add tcp test.

--- src/moudle/CommandLineParse/Program.cs
+++ src/moudle/CommandLineParse/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tools.DataHelper;
 
 namespace CommandLineParse
 {
@@ -11,21 +12,25 @@ namespace CommandLineParse
     {
         static void Main(string[] args)
         {
-            CommandLine.Parser.Default.ParseArguments<Options>(args)
-            .WithParsed<Options>(opts => RunOptionsAndReturnExitCode(opts))
-            .WithNotParsed<Options>((errs) => HandleParseError(errs));
+            Parser.Default.ParseArguments<Options>(args).WithParsed(Run);
             Console.ReadKey();
         }
 
-        static int RunOptionsAndReturnExitCode(Options options)
+        static void Run(Options options)
         {
-            
-            return 0;
+            if (options.File.IsEmpty())
+            {
+                Log.Error("error.");
+            }
+            else
+            {
+                Log.Info(options.File);
+            }
         }
 
         static void HandleParseError(IEnumerable<Error> errs)
         {
-
+            ;
         }
 
     }
@@ -33,18 +38,11 @@ namespace CommandLineParse
 
     class Options
     {
-        [Option('s', "source", MetaValue = "FILE", Required = true, HelpText = "输入数据文件")]
-        public string InputFile { get; set; }
+        [Option('f', "file", Required = true, HelpText = "需要处理的文件。")]
+        public string File { get; set; }
 
-        [Option('t', "target", MetaValue = "FILE", Required = false, HelpText = "输出数据文件")]
-        public string OutputFile { get; set; }
-
-
-        [Option('s', "start-time", MetaValue = "STARTTIME", Required = true, HelpText = "开始时间")]
-        public DateTime StartTime { get; set; }
-
-        [Option('e', "end-time", MetaValue = "ENDTIME", Required = true, HelpText = "结束时间")]
-        public DateTime EndTime { get; set; }
+        [Option('o', "override", Required = false, HelpText = "是否覆盖原有文件。")]
+        public bool Override { get; set; }
 
 
     }
---
+++ src/moudle/TCPTest/http/Download.cs
@@ -0,0 +1,59 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Tools.DataHelper;
+
+namespace TCPTest.http
+{
+    class Download
+    {
+
+        /// <summary>
+        /// 获取网页的HTML码
+        /// </summary>
+        /// <param name="url">链接地址</param>
+        /// <param name="encoding">编码类型</param>
+        /// <returns></returns>
+        public static string GetHtmlStr(string url, string encoding)
+        {
+            string htmlStr = "";
+            if (!String.IsNullOrEmpty(url))
+            {
+                WebRequest request = WebRequest.Create(url);            //实例化WebRequest对象
+                WebResponse response = request.GetResponse();           //创建WebResponse对象
+                Stream datastream = response.GetResponseStream();       //创建流对象
+                Encoding ec = Encoding.Default;
+                if (encoding == "UTF8")
+                {
+                    ec = Encoding.UTF8;
+                }
+                else if (encoding == "Default"||encoding.IsEmpty())
+                {
+                    ec = Encoding.Default;
+                }
+                StreamReader reader = new StreamReader(datastream, ec);
+                htmlStr = reader.ReadToEnd();                           //读取数据
+                reader.Close();
+                datastream.Close();
+                response.Close();
+            }
+            return htmlStr;
+        }
+
+        static void Main(string[] args)
+        {
+            var html=GetHtmlStr("http://eid.csrc.gov.cn/fund/web/html_view.instance?instanceid=5839599", "UTF8");
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            var nodes = doc.DocumentNode.SelectNodes("/html/body/table");
+            nodes = doc.DocumentNode.SelectNodes("//table");
+            Console.WriteLine(html);
+        }
+
+    }
+}
